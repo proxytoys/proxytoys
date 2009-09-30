922126302817fbdf0457fa58ebcd647cc346f0f1
tsdeng <tsdeng>
2009-09-30 04:11:51 +0000

    changed Dispatching Toy Test to using mockito and junit4.
    git-svn-id: https://svn.codehaus.org/proxytoys/trunk@201 c3c2784b-8cd7-0310-8e00-a8c905b2d8b3

--- proxytoys/src/test/java/com/thoughtworks/proxy/toys/dispatch/DispatchingTest.java
+++ proxytoys/src/test/java/com/thoughtworks/proxy/toys/dispatch/DispatchingTest.java
@@ -7,21 +7,24 @@
  */
 package com.thoughtworks.proxy.toys.dispatch;
 
-import com.thoughtworks.proxy.ProxyTestCase;
-import static com.thoughtworks.proxy.toys.dispatch.Dispatching.dispatchable;
-import com.thoughtworks.proxy.kit.NoOperationResetter;
+import com.thoughtworks.proxy.NewProxyTestCase;
 import com.thoughtworks.proxy.kit.Resetter;
-import org.jmock.Mock;
+import com.thoughtworks.proxy.kit.NoOperationResetter;
+import static com.thoughtworks.proxy.toys.dispatch.Dispatching.dispatchable;
+import static junit.framework.Assert.*;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.IOException;
 
 
 /**
  * @author J&ouml;rg Schaible
  */
-public class DispatchingTest extends ProxyTestCase {
+public class DispatchingTest extends NewProxyTestCase {
 
     public interface Foo {
         String getSomething();
@@ -42,104 +45,125 @@ public class DispatchingTest extends ProxyTestCase {
     public interface FooBar extends Foo, Bar {
     }
 
-    public void testCallsAreDispatchedBetweenObjects() throws Exception {
-        Mock fooMock = mock(Foo.class);
-        Mock barMock = mock(Bar.class);
+    @Test
+    public void callsAreDispatchedBetweenObjects() throws Exception {
+        Foo fooMock = mock(Foo.class);
+        Bar barMock = mock(Bar.class);
 
         Object foobar = dispatchable(new Class[]{Foo.class, Bar.class}, new Object[]{
-                fooMock.proxy(), barMock.proxy()}).build(getFactory());
+                fooMock, barMock}).build(getFactory());
 
-        fooMock.expects(once()).method("getSomething").withNoArguments().will(returnValue("some thing"));
-        barMock.expects(once()).method("doSomething").with(eq("some thing"));
-
+        when(fooMock.getSomething()).thenReturn("some thing");
         assertEquals("some thing", ((Foo) foobar).getSomething());
         ((Bar) foobar).doSomething("some thing");
+
+        //verify
+        verify(fooMock).getSomething();
+        verify(barMock).doSomething(eq("some thing"));
+
+
     }
 
     // This is a proxy limitation ... unfortunately
-    public void testOnlyFirstOfMethodsWithSameSignaturesIsCalled() throws Exception {
-        Mock fooMock = mock(Foo.class);
-        Mock fooMimicMock = mock(FooMimic.class);
+    @Test
+    public void onlyFirstOfMethodsWithSameSignaturesIsCalled() throws Exception {
+        //setup
+        Foo fooMock = mock(Foo.class);
+        FooMimic fooMimicMock = mock(FooMimic.class);
 
         Object foo = dispatchable(new Class[]{Foo.class, FooMimic.class}, new Object[]{
-                fooMock.proxy(), fooMimicMock.proxy()}).build(getFactory());
-
-        fooMock.expects(once()).method("getSomething").withNoArguments().will(returnValue("some thing"));
-        // should be fooMimicMock ...
-        fooMock.expects(once()).method("getSomething").withNoArguments().will(returnValue("some thing"));
-
+                fooMock, fooMimicMock}).build(getFactory());
+        //expects
+        when(fooMock.getSomething()).thenReturn("some thing");
+        when(fooMimicMock.getSomething()).thenReturn("some thing");
+        //verify
         assertEquals("some thing", ((Foo) foo).getSomething());
-        assertEquals("some thing", ((FooMimic) foo).getSomething());
+        assertEquals("some thing", (fooMimicMock).getSomething());
+        verify(fooMock).getSomething();
+        verify(fooMimicMock).getSomething();
     }
 
-    public void testMethodsWithSameNameButDifferentSignatureAreDistinct() throws Exception {
-        Mock barMock = mock(Bar.class);
-        Mock barSimilarMock = mock(BarSimilar.class);
+    @Test
+    public void methodsWithSameNameButDifferentSignatureAreDistinct() throws Exception {
+        //setup
+        Bar barMock = mock(Bar.class);
+        BarSimilar barSimilarMock = mock(BarSimilar.class);
 
         Object bar = dispatchable(new Class[]{Bar.class, BarSimilar.class}, new Object[]{
-                barMock.proxy(), barSimilarMock.proxy()}).build(getFactory());
-
-        barMock.expects(once()).method("doSomething").with(eq("some thing"));
-        barSimilarMock.expects(once()).method("doSomething").with(eq(1));
+                barMock, barSimilarMock}).build(getFactory());
+        //expects
 
         ((Bar) bar).doSomething("some thing");
         ((BarSimilar) bar).doSomething(1);
+        //verify
+
+        verify(barMock).doSomething(eq("some thing"));
+        verify(barSimilarMock).doSomething(eq(1));
     }
 
-    public void testOneDelegateCanMatchMultipleTypes() throws Exception {
-        Mock fooBarMock = mock(FooBar.class);
+    @Test
+    public void oneDelegateCanMatchMultipleTypes() throws Exception {
+        //setup
+        FooBar fooBarMock = mock(FooBar.class);
 
         Object foobar = dispatchable(
-                new Class[]{Foo.class, Bar.class}, new Object[]{fooBarMock.proxy()}).build(getFactory());
+                new Class[]{Foo.class, Bar.class}, new Object[]{fooBarMock}).build(getFactory());
+        //expects
 
-        fooBarMock.expects(once()).method("doSomething").with(eq("some thing"));
-        fooBarMock.expects(once()).method("getSomething").withNoArguments().will(returnValue("some thing"));
+        when(fooBarMock.getSomething()).thenReturn("some thing");
 
+        //verify
         assertEquals("some thing", ((Foo) foobar).getSomething());
         ((Bar) foobar).doSomething("some thing");
+        verify(fooBarMock).doSomething(eq("some thing"));
+        verify(fooBarMock).getSomething();
     }
 
-    public void testAllTypesMustBeMatchedByOneDelegate() throws Exception {
-        Mock fooMock = mock(Foo.class);
+    @Test
+    public void allTypesMustBeMatchedByOneDelegate() throws Exception {
+        Foo fooMock = mock(Foo.class);
 
         try {
-            dispatchable(new Class[]{Foo.class, Bar.class}, new Object[]{fooMock.proxy()}).build(getFactory());
+            dispatchable(new Class[]{Foo.class, Bar.class}, new Object[]{fooMock}).build(getFactory());
             fail("DispatchingException expected");
         } catch (final DispatchingException e) {
             assertEquals(Bar.class, e.getType());
         }
     }
 
-    public void testHashCodeIsDifferentForEachProxy() throws Exception {
-        Mock fooMock = mock(Foo.class);
+    @Test
+    public void hashCodeIsDifferentForEachProxy() throws Exception {
+        Foo fooMock = mock(Foo.class);
 
-        Object proxy1 = dispatchable(new Class[]{Foo.class}, new Object[]{fooMock.proxy()}).build( getFactory());
-        Object proxy2 = dispatchable(new Class[]{Foo.class}, new Object[]{fooMock.proxy()}).build( getFactory());
+        Object proxy1 = dispatchable(new Class[]{Foo.class}, new Object[]{fooMock}).build(getFactory());
+        Object proxy2 = dispatchable(new Class[]{Foo.class}, new Object[]{fooMock}).build(getFactory());
 
         assertFalse(proxy1.hashCode() == proxy2.hashCode());
     }
 
-    public void testStringRepresentationContainsImplementedTypes() throws Exception {
-        Mock fooBarMock = mock(FooBar.class);
+    @Test
+    public void tringRepresentationContainsImplementedTypes() throws Exception {
+        FooBar fooBarMock = mock(FooBar.class);
 
         Object foobar = dispatchable(
-                new Class[]{Foo.class, Bar.class}, new Object[]{fooBarMock.proxy()}).build( getFactory());
+                new Class[]{Foo.class, Bar.class}, new Object[]{fooBarMock}).build(getFactory());
 
         String string = foobar.toString();
         assertTrue(string.indexOf(Foo.class.getName()) >= 0);
         assertTrue(string.indexOf(Bar.class.getName()) >= 0);
     }
 
-    public void testTwoProxiesAreEqualIfSameTypesAreDelegatedToEqualDelegates() throws Exception {
+    @Test
+    public void twoProxiesAreEqualIfSameTypesAreDelegatedToEqualDelegates() throws Exception {
         Object proxy1 = dispatchable(new Class[]{Comparable.class, Runnable.class, List.class}, new Object[]{
-                new ArrayList(), "Hello", Thread.currentThread()}).build( getFactory());
+                new ArrayList(), "Hello", Thread.currentThread()}).build(getFactory());
         Object proxy2 = dispatchable(new Class[]{List.class, Runnable.class, Comparable.class}, new Object[]{
-                "Hello", new ArrayList(), Thread.currentThread()}).build( getFactory());
+                "Hello", new ArrayList(), Thread.currentThread()}).build(getFactory());
 
         assertEquals(proxy1, proxy2);
     }
-
-    public void testTwoProxiesAreNotEqualIfSameTypesAreDelegatedToAtLeastOneNonEqualDelegate() throws Exception {
+    @Test
+    public void twoProxiesAreNotEqualIfSameTypesAreDelegatedToAtLeastOneNonEqualDelegate() throws Exception {
         Object proxy1 = dispatchable(new Class[]{Comparable.class, Runnable.class, List.class}, new Object[]{
                 new ArrayList(), "Foo", Thread.currentThread()}).build( getFactory());
         Object proxy2 = dispatchable(new Class[]{List.class, Runnable.class, Comparable.class}, new Object[]{
@@ -152,16 +176,18 @@ public class DispatchingTest extends ProxyTestCase {
         assertTrue(resetter.reset(this));
     }
 
-    public void testSerializeWithJDK() throws IOException, ClassNotFoundException {
+    @Test
+    public void serializeWithJDK() throws IOException, ClassNotFoundException {
         useSerializedProxy((Resetter) serializeWithJDK(dispatchable(
                 new Class[]{Resetter.class}, new Object[]{new NoOperationResetter()}).build( getFactory())));
     }
-
-    public void testSerializeWithXStream() {
+    @Test
+    public void serializeWithXStream() {
         useSerializedProxy((Resetter) serializeWithXStream(dispatchable(
                 new Class[]{Resetter.class}, new Object[]{new NoOperationResetter()}).build(getFactory())));
     }
 
+    @Test
     public void testSerializeWithXStreamInPureReflectionMode() {
         useSerializedProxy((Resetter) serializeWithXStreamAndPureReflection(dispatchable(
                 new Class[]{Resetter.class}, new Object[]{new NoOperationResetter()}).build(getFactory())));
