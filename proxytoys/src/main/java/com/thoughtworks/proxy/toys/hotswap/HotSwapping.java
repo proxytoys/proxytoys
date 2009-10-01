140187ae81dbcf969fbe522c0aa57c91f962eb98
Paul Hammant (paul) <phammant@thoughtworks.com>
2009-10-01 10:52:12 +0000

    some builder method chaining ordering for hotswapping
    git-svn-id: https://svn.codehaus.org/proxytoys/trunk@215 c3c2784b-8cd7-0310-8e00-a8c905b2d8b3

--- proxytoys/src/main/java/com/thoughtworks/proxy/toys/hotswap/HotSwapping.java
+++ proxytoys/src/main/java/com/thoughtworks/proxy/toys/hotswap/HotSwapping.java
@@ -39,34 +39,8 @@ public class HotSwapping<T> {
      * @param type the type of the proxy when it is finally created.
      * @return a factory that will proxy instances of the supplied type.
      */
-    public static <T> HotSwapping<T> hotSwappable(final Class<T> type) {
-        return new HotSwapping<T>(type);
-    }
-
-    /**
-     * Defines the object that shall be proxied. This delegate must implement the types used to create the hot swap or
-     * have signature compatible methods.
-     *
-     * @param instance the object that shall be proxied.
-     * @return the factory that will proxy instances of the supplied type.
-     */
-    public HotSwapping<T> with(final Object instance) {
-        this.instance = instance;
-        delegationMode = type.isInstance(instance) ? DIRECT : SIGNATURE;
-        return this;
-    }
-
-    /**
-     * Forces a particular delegation mode to be used.
-     *
-     * @param delegationMode refer to {@link DelegationMode#DIRECT} or
-     *                       {@link DelegationMode#SIGNATURE} for allowed
-     *                       values.
-     * @return the factory that will proxy instances of the supplied type.
-     */
-    public HotSwapping mode(DelegationMode delegationMode) {
-        this.delegationMode = delegationMode;
-        return this;
+    public static <T> HotSwappingWith<T> hotSwappable(final Class<T> type) {
+        return new HotSwappingWith<T>(type);
     }
 
     /**
@@ -78,8 +52,76 @@ public class HotSwapping<T> {
      * @param factory the @{link ProxyFactory} to use.
      * @return the created proxy implementing the <tt>types</tt> and {@link Swappable}
      */
-    public T build(final ProxyFactory factory) {
+    private T build(final ProxyFactory factory) {
         final ObjectReference delegateReference = new SimpleReference(instance);
         return (T) new HotSwappingInvoker(new Class[]{type}, factory, delegateReference, delegationMode).proxy();
     }
+
+    public static class HotSwappingWith<T> {
+        private final HotSwapping<T> hotswapping;
+
+        public HotSwappingWith(Class<T> type) {
+            this.hotswapping = new HotSwapping(type);
+        }
+
+        /**
+         * Defines the object that shall be proxied. This delegate must implement the types used to create the hot swap or
+         * have signature compatible methods.
+         *
+         * @param instance the object that shall be proxied.
+         * @return the factory that will proxy instances of the supplied type.
+         */
+        public HotSwappingBuildOrMode<T> with(final Object instance) {
+            hotswapping.instance = instance;
+            hotswapping.delegationMode = hotswapping.type.isInstance(instance) ? DIRECT : SIGNATURE;
+            return new HotSwappingBuildOrMode(hotswapping);
+        }
+
+    }
+
+    public static class HotSwappingBuildOrMode<T> extends HotSwappingBuild<T>{
+        public HotSwappingBuildOrMode(HotSwapping hotswapping) {
+            super(hotswapping);
+        }
+
+        /**
+         * Forces a particular delegation mode to be used.
+         *
+         * @param delegationMode refer to {@link DelegationMode#DIRECT} or
+         *                       {@link DelegationMode#SIGNATURE} for allowed
+         *                       values.
+         * @return the factory that will proxy instances of the supplied type.
+         */
+        public HotSwappingBuild mode(DelegationMode delegationMode) {
+            hotswapping.delegationMode = delegationMode;
+            return new HotSwappingBuild(hotswapping);
+        }
+
+
+    }
+
+    public static class HotSwappingBuild<T> {
+        protected final HotSwapping<T> hotswapping;
+
+        public HotSwappingBuild(HotSwapping hotswapping) {
+            this.hotswapping = hotswapping;
+        }
+        
+        /**
+         * Create a proxy with hot swapping capabilities for specific types of the delegate given with an
+         * {@link ObjectReference}. The delegate must implement the given types, if the invoker is in static typing mode,
+         * otherwise it must only have signature compatible methods. Proxies created by this method will implement
+         * {@link Swappable}
+         *
+         * @param factory the @{link ProxyFactory} to use.
+         * @return the created proxy implementing the <tt>types</tt> and {@link Swappable}
+         */
+        public T build(final ProxyFactory factory) {
+            return hotswapping.build(factory);
+        }
+
+    }
+
+
+
 }
