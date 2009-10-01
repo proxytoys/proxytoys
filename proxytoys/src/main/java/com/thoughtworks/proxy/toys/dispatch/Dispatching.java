ef80417218d76f76e3e9617d7ba65a52d549a5bb
Paul Hammant (paul) <phammant@thoughtworks.com>
2009-10-01 10:42:43 +0000

    some builder method chaining ordering for dispatching
    git-svn-id: https://svn.codehaus.org/proxytoys/trunk@214 c3c2784b-8cd7-0310-8e00-a8c905b2d8b3

--- proxytoys/src/main/java/com/thoughtworks/proxy/toys/dispatch/Dispatching.java
+++ proxytoys/src/main/java/com/thoughtworks/proxy/toys/dispatch/Dispatching.java
@@ -23,58 +23,26 @@ import static com.thoughtworks.proxy.toys.delegate.DelegationMode.SIGNATURE;
  * @since 0.2
  */
 public class Dispatching {
+
     private Class[] types;
     private Object[] delegates;
-
 
     private Dispatching(Class[] types) {
         this.types = types;
     }
 
     /**
-     * Creates a factory for proxy instances that allow delegation.
+     * Creates a builder for proxy instances that allow delegation.
      *
      * @param types     the types of the proxy
-     * @return a factory that will proxy instances of the supplied type.
-     * @since 0.2
+     * @return a builder that will proxy instances of the supplied type.
      */
-
-    public static Dispatching dispatchable(Class... types) {
-        return new Dispatching(types);
+    public static DispatchingWith dispatchable(Class... types) {
+        return new DispatchingWith(types);
 
     }
 
-    /**
-     * Defines the object that shall be delegated to. This delegate must implement the types used to create the hot swap or
-     * have signature compatible methods.
-     *
-     * @param delegates the objects that will receive the calls.
-     * @return the factory that will proxy instances of the supplied type.
-     */
-    public Dispatching with(final Object... delegates) {
-        this.delegates = delegates;
-        return this;
-    }
-
-
-    /**
-     * * Create a dispatching proxy of given types for the given objects with {@link StandardProxyFactory}
-     *
-     * @return the created proxy
-     * @since 0.2
-     */
-    public Object build() {
-        return build(new StandardProxyFactory());
-    }
-
-    /**
-     * * Create a dispatching proxy of given types for the given objects.
-     *
-     * @param factory the {@link ProxyFactory} to use
-     * @return the created proxy
-     * @since 0.2
-     */
-    public Object build(ProxyFactory factory) {
+    private Object build(ProxyFactory factory) {
 
         final ObjectReference[] references = new ObjectReference[delegates.length];
         for (int i = 0; i < references.length; i++) {
@@ -82,4 +50,57 @@ public class Dispatching {
         }
         return factory.createProxy(types, new DispatchingInvoker(factory, types, references));
     }
+
+    public static class DispatchingWith {
+        private final Dispatching dispatching;
+
+        private DispatchingWith(Class[] types) {
+            this.dispatching = new Dispatching(types);
+        }
+
+        /**
+         * Defines the object that shall be delegated to. This delegate must implement the types used to create the hot swap or
+         * have signature compatible methods.
+         *
+         * @param delegates the objects that will receive the calls.
+         * @return the factory that will proxy instances of the supplied type.
+         */
+        public DispatchingBuild with(final Object... delegates) {
+            dispatching.delegates = delegates;
+            return new DispatchingBuild(dispatching);
+        }
+    }
+
+    public static class DispatchingBuild {
+        private final Dispatching dispatching;
+
+
+        private DispatchingBuild(Dispatching dispatching) {
+
+            this.dispatching = dispatching;
+        }
+
+        /**
+         * * Create a dispatching proxy of given types for the given objects with {@link StandardProxyFactory}
+         *
+         * @return the created proxy
+         */
+        public Object build() {
+            return build(new StandardProxyFactory());
+        }
+
+        /**
+         * * Create a dispatching proxy of given types for the given objects.
+         *
+         * @param factory the {@link ProxyFactory} to use
+         * @return the created proxy
+         */
+        public Object build(ProxyFactory factory) {
+            return dispatching.build(factory);
+        }
+
+
+    }
+
+
 }
